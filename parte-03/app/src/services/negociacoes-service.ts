83be6d694217c816f1f8a237512e71a437f7368a
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-06-26 19:12:05 -0300

    feat: :sparkles: Consumo de API externa, definição de interface, e isolamento do acesso à API em um serviço.

---
+++ parte-03/app/src/services/negociacoes-service.ts
@@ -0,0 +1,16 @@
+import { NegociacoesDoDia } from "../interfaces/negociacao-do-dia.js";
+import { Negociacao } from "../models/negociacao.js";
+
+export class NegociacoesService {
+    public obteNegociacoesDoDia(): Promise<Negociacao[]> {
+        return fetch('http://localhost:8080/dados')
+            .then((res) => {
+                return res.json();    
+            })
+            .then((dados: NegociacoesDoDia[]) => {
+                return dados.map((dadoDeHoje) => {
+                    return new Negociacao(new Date(), dadoDeHoje.vezes, dadoDeHoje.montante);
+                })
+            }) 
+    }
+}
