7fc6d4434dd1637ce8a2d6c8d0c313ed6f41a37c
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-06-26 15:13:06 -0300

    feat: :sparkles: Evitar código duplicado, decorator de propriedade, criação dinâmica de getters com Object.defineProperty, e cache de decorators.

--- parte-03/app/src/views/view.ts
+++ parte-03/app/src/views/view.ts
@@ -14,8 +14,8 @@ export abstract class View<T> {
         }
     }
     
-    @timeExec()
-    @inspect
+    /* @timeExec()
+    @inspect */
     public update(model: T): void {
         let template = this.template(model);
         this.elemento.innerHTML = template;
