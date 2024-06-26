b983e16dc3d3667e8d978e935fd128916143606a
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-06-26 10:21:34 -0300

    refactor: :recycle: Refatoração da criação da negociação

--- parte-02/dist/js/controllers/negociacao-controller.js
+++ parte-02/dist/js/controllers/negociacao-controller.js
@@ -14,7 +14,7 @@ export class NegociacaoController {
         this.negociacoesView.update(this.negociacoes);
     }
     adiciona() {
-        const negociacao = this.criaNegociacao();
+        const negociacao = Negociacao.criarNegociacao(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
         if (!this.verificarDiaUtil(negociacao.data)) {
             this.mensagemView
                 .update('Apenas negociações em dias úteis são aceitas.');
@@ -23,13 +23,6 @@ export class NegociacaoController {
         this.negociacoes.adiciona(negociacao);
         this.limparFormulario();
         this.atualizaView();
-    }
-    criaNegociacao() {
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date, quantidade, valor);
     }
     limparFormulario() {
         this.inputData.value = '';
