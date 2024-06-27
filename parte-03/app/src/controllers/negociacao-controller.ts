feb66dac8aafe363129f09869a51638bd4c61c8b
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-06-26 22:09:08 -0300

    feat: :sparkles: Eliminação de múltiplos console.log, aplicação prática do polimorfismo, interface de método, prevenção de negociações duplicadas e extensão de interfaces.

--- parte-03/app/src/controllers/negociacao-controller.ts
+++ parte-03/app/src/controllers/negociacao-controller.ts
@@ -5,6 +5,7 @@ import { DiasDaSemana } from '../enums/dias-da-semana.js';
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
 import { NegociacoesService } from '../services/negociacoes-service.js';
+import { imprimir } from '../utils/imprimir.js';
 import { MensagemView } from '../views/mensagem-view.js';
 import { NegociacoesView } from '../views/negociacoes-view.js';
 
@@ -41,14 +42,21 @@ export class NegociacaoController {
                 .update('Apenas negociações em dias úteis são aceitas');
             return ;
         }
-
+        imprimir(negociacao, this.negociacoes);
         this.negociacoes.adiciona(negociacao);
         this.limparFormulario();
         this.atualizaView();
     }
 
     public importaDados(): void {
-        this.negociacoesService.obteNegociacoesDoDia()
+        this.negociacoesService.obteNegociacoesDoDia()  
+        .then((negociacoesDeHoje) => {
+            return negociacoesDeHoje.filter((negociacaoDeHoje) => {
+                return !this.negociacoes
+                    .lista()
+                    .some((negociacao) => negociacao.ehIgual(negociacaoDeHoje));
+            })
+        })
         .then((negociacoesDeHoje: Negociacao[]) => {
             for(let negociacao of negociacoesDeHoje) {
                 this.negociacoes.adiciona(negociacao);
